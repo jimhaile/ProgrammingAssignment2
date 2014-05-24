da71a1841da112b42a08bb8e61e965212541178b
Jim Haile <jim@Jims-MacBook-Air.local>
2014-05-24 12:57:30 -0700

    My first pass

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculates the inverse of a square matrix
+## create a square matrix and pass it to makeCacheMatrix
+## c <- matrix(1:4,2,2)
+## a <- makeCacheMatrix(c)
+##
+## now send 'a' to cacheSolve
+##  b <- cacheSolve(a)
+##  b now is a 2x2 inverse of c
+# to prove it...
+## c %*% b 
+## returns to identity matrix
 
-## Write a short comment describing this function
+## This function takes a square matrix and stores it internally
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) m <<- solve
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+## This function uses solve to find the inverse of the 
+## internally stored matrix in makeCacheMatrix
+## and calculates the inverse by using solve()
+## if the inverse is not cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
